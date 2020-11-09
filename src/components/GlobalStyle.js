b02ff5b95e0e6b1165fe91c066cdb2bd6515c8b3
Rob Gleason <51282111+Epiq122@users.noreply.github.com>
2020-11-08 20:45:31 -0700

    6. Added Styled Components  -Services Section  -created a style template js for styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -8,16 +8,19 @@ const GlobalStyle = createGlobalStyle`
 }
 body{
   background:#1b1b1b;
+  font-family:'Inter',sans-serif
   
 }
 button{
   font-weight:bold;
+  padding:1rem 2rem;
   font-size:1.1rem;
   cursor:pointer;
   border:3px solid #23d997;
   background:transparent;
   color:white;
   transition: all 0.5s ease;
+  font-family:'Inter',sans-serif
 &:hover{
   background-color:#23d997;
   color:white;
@@ -37,18 +40,19 @@ h4{
   font-weight:bold;
  
 }
-p{
-  padding:3rem 0rem
-  color:#ccc;
-  font-size:1.4rem;
-  line-height:150%
-}
+
 span{
   font-weight:bold;
   color:#23d997;
 }
 a{
   font-size:1.1rem;
+}
+p{
+  padding:3rem 0rem;
+  color:#ccc;
+  font-size:1.4rem;
+  line-height:150%
 }
   `;
 
