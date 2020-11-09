a3ae7091246c53d9169aa6405f4db3a5b11285e8
Rob Gleason <51282111+Epiq122@users.noreply.github.com>
2020-11-08 19:41:34 -0700

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ServicesSection from '../components/ServicesSection';
 import AboutSection from '../components/AboutSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
