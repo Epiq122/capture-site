ab2f23c433d93cb816b01f887be262e420936038
Rob Gleason <51282111+Epiq122@users.noreply.github.com>
2020-11-08 21:08:10 -0700

    8. Created NavBar and Added Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import NavBar from './components/NavBar';
 import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <GlobalStyle />
       <AboutUs />
     </div>
